b7d0117f3b302035d6c28b914c9c6d1726d78de5
ylambers <y.lambers@outlook.com>
2018-04-04 20:53:34 +0200

    Added TimeManger and TimeMode

---
+++ SmartWatch/TimeManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartWatch
+{
+    class TimeManager
+    {
+        public TimeManger()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void IncrementHours()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void IncrementMinutes()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void IncrementSeconds()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ SmartWatch/TimeMode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartWatch
+{
+    class TimeMode : IMode
+    {
+        public TimeMode()
+        {
+            var t = new Time();
+
+        }
+     
+        public int currentCase { get; set; }
+        public Time time { get; set; }
+        public void Mode(Time time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void buttonOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void buttonTwo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void switchCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawGui(Panel modePanel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void tick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayTime()
+        {
+
+        }
+    }
+}
